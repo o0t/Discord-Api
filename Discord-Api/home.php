74bc3c5973fa093ca400f673802cec4f71140246
o0t <rayanalfasl9@gmail.com>
2022-07-30 04:05:42 +0300

    #

---
+++ Discord-Api/home.php
@@ -0,0 +1,36 @@
+<?php 
+    session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title> - Dashboard - </title>
+  </head>
+  <body>
+            <br><br>
+        
+        <div class="container">
+        <?php 
+                echo "<pre>";
+                print_r($_SESSION['user']);
+                echo "</pre>";
+        ?>
+        
+            <a href="exit.php" class="btn btn-outline-secondary">LogOut</a>
+        </div>
+
+        
+
+        
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Discord-Api/url-oauth2.php
@@ -0,0 +1,7 @@
+<?php
+
+$discord_url = "https://discord.com/api/oauth2/authorize?client_id="; // GENERATED URL
+header("Location: $discord_url");
+exit();
+
+?>
